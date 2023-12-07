52036a4309b7c3c9cfa393e1cf0b068035a9a394
Kungjace <jacob.hedman@elev.ga.ntig.se>
2023-12-07 14:33:30 +0100

    Hej

---
+++ GroupNames.ps1
@@ -0,0 +1,9 @@
+﻿$users = Import-Csv GroupNames.csv | select *, username
+
+
+foreach($user in $users) {
+    $user.username = $user.firstname + "." + $user.lastname + "." + "NTI"
+
+}
+
+Write-Output "New-ADUser -Name "$user.username" -Path "OU=LightSeaGreen,DC=colors,DC=labb""
